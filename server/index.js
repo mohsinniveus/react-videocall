0d059d31338c2e498258bfcf36e7103ed5bc3871
mohsinniveus <mohsin.khan@niveussolutions.com>
2020-08-14 00:06:17 +0530

    new changes

--- server/index.js
+++ server/index.js
@@ -1,17 +1,31 @@
-const https = require('https')
-const express = require('express');
-const config = require('../config');
+var express = require('express');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
 const socket = require('./lib/socket');
 
-const app = express();
-const server = https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-},app);
+// This line is from the Node.js HTTPS documentation.
+var options = {
+  key: fs.readFileSync('/home/webrtc/react-videocall/server/server.key'),
+  cert: fs.readFileSync('/home/webrtc/react-videocall/server/server.cert')
+};
+
+// Create a service (the app object is just a callback).
+var app = express();
 
 app.use('/', express.static(`${__dirname}/../client`));
 
-server.listen(config.PORT, () => {
-  socket(server);
-  console.log('Server is listening at :', config.PORT);
+
+var server = https.createServer({
+    key: fs.readFileSync('/home/webrtc/react-videocall/server/server.key'),
+    cert: fs.readFileSync('/home/webrtc/react-videocall/server/server.cert'),
+    requestCert: false,
+    rejectUnauthorized: false
+},app);
+server.listen(5000);
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection',function (socket) {
+    console.log("socket connected");
 });
